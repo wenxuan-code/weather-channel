69ca1cd66993a8d66997f6dbea934fa39647d098
muahchee <187490843+muahchee@users.noreply.github.com>
2026-03-20 16:16:49 +1300

    Build: input validation and 500 page
    Not really sure what to validate tbh.
    I basically just trimmed the input.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,64 +1,78 @@
 import dotenv from "dotenv";
-import NodeGeocoder from "node-geocoder";
 dotenv.config();
+import NodeGeocoder from "node-geocoder";
+import { body, matchedData, validationResult } from "express-validator";
+import dummygeo from "../dev-reference/dummygeo.json" with { type: "json" };
 
 const apiKey = process.env.API_KEY;
 const geoApiKey = process.env.GEO_API_KEY;
 const geoOptions = {
   provider: "tomtom",
   apiKey: geoApiKey,
-  // language: "english",
-  // email: "pipis2@keemail.me"
 };
 const geocoding = NodeGeocoder(geoOptions);
 
-export const searchLocation = (req, res) => {
-  const location = req.body.location;
+const validateSearch = [
+  body("location")
+    .trim()
+    .isString()
+    .withMessage("Location should be a string")
+];
 
-  res.redirect(`/?location=${location}`);
-};
+//for the form input
+export const searchLocation = [
+  validateSearch,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("index", {
+        success: false,
+        errors: errors.array(),
+        input: req.body.location,
+      });
+    }
+    const { location } = matchedData(req);
 
+    res.redirect(`/?location=${location}`);
+  },
+];
+
+//index page
 export async function displayWeather(req, res) {
   //on init or empty query, default to "auckland"
   const location = req.query.location || "Auckland";
-
-  console.log(location)
-
   if (location === "") {
     location = "Auckland";
   }
 
   // Convert address to coordinates
-  const coordinates = await geocoding.geocode(location);
+  // const coordinates = await geocoding.geocode(location);
+
+  //!DUMMY coord
+  const coordinates = dummygeo;
 
   //if coordinates.length < 1, res.render index with a error
   if (coordinates.length < 1) {
     res.status(500).render("index", {
       success: false,
       errors: [
-        { msg: "Can't find specified location. You might have made a type!" },
+        { msg: "Can't find specified location. You might have made a typo!" },
       ],
     });
-  } else if (!coordinates) {
-    res.status(500).render("index", {
-      success: false,
-      errors: [{ msg: "Can't connect to geocoder" }],
-    });
+    return;
   }
 
   console.log("Coordinates:", coordinates);
 
   const finalCoord = coordinates[0];
 
-  console.log(finalCoord.state);
-
   res.render("index", {
     success: true,
-    location:
-      finalCoord.state
-        ? ` ${finalCoord.state}, ${finalCoord.country}`
-        : `${location}, ${finalCoord.country}`,
+    location: finalCoord.state
+      ? ` ${finalCoord.state}, ${finalCoord.country}`
+      : `${location}, ${finalCoord.country}`,
     longitude: finalCoord.longitude,
     latitude: finalCoord.latitude,
+    input: location,
   });
 }
