a7b765f47c6d003026e52c41724654dd9ec18b30
muahchee <187490843+muahchee@users.noreply.github.com>
2026-03-22 20:11:58 +1300

    Build: next arrow for textbox, draft sprites
    - Added 3/4 draft sprites for tellie
    
    - Add next arrow to bottom right of textbox,
    with bobbing animation

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -3,32 +3,45 @@ function fillReport({ currently, hourly, daily, datetime, location }) {
     {
       text: `Kia ora <span class="bold">${location}</span>! It is currently <span class="bold">${datetime}.</span>`,
       icon: "wizard.png",
-      alt: "wizard weather forecast logo"
+      alt: "wizard weather forecast logo",
+      tellieImg: "tellie.gif",
+      tellieAlt: ""
+
     },
     {
       text: `You're watching the <span class="bold">Wizard Weather Forecast</span>. I am your reporter <span class="bold">Tellie</span>, bringing you the latest weather update.`,
       icon: "wizard.png",
-      alt: "wizard weather forecast logo"
+      alt: "wizard weather forecast logo",
+      tellieImg: "tellie.gif",
+      tellieAlt: ""
     },
     {
       text: `Right now, the conditions are <span class="bold">"${currently.condition.toLowerCase()}"</span>. It feels like <span class="bold">${currently.temperature}°C</span>.`,
       icon: `${currently.icon}.png`,
-      alt: currently.icon
+      alt: currently.icon,
+      tellieImg: "tellie-currently.gif",
+      tellieAlt: ""
     },
     {
       text: `For the next few hours, conditions are <span class="bold">"${hourly.condition.toLowerCase()}"</span>`,
       icon: `${hourly.icon}.png`,
-      alt: hourly.icon
+      alt: hourly.icon,
+      tellieImg: "tellie-currently.gif",
+      tellieAlt: ""
     },
     {
       text: `Our predictions for the coming week are "<span class="bold">${daily.condition.toLowerCase()}</span>"`,
       icon: `${daily.icon}.png`,
-      alt: daily.icon
+      alt: daily.icon,
+      tellieImg: "tellie-daily.gif",
+      tellieAlt: ""
     },
     {
       text: `...And that's it for the weather report. Thanks for tuning in!`,
       icon: "wizard.png",
-      alt: "wizard weather forecast logo"
+      alt: "wizard weather forecast logo",
+      tellieImg: "tellie.gif",
+      tellieAlt: ""
     },
   ];
 
@@ -60,11 +73,12 @@ export async function click(req, res) {
           <img src="./images/weather-icons/${report[index].icon}" alt="${report[index].alt}">
         </div>
         <div class="tv__reporter">
-          <img src="./images/tellie.gif" alt="">
+          <img src="./images/${report[index].tellieImg}" alt="${report[index].tellieAlt}">
         </div>
       </div>
       <div class="tv_bottom">
         <p>${report[index].text}</p>
+        <svg class="next-arrow" viewBox="0 0 24 24"><path d="M1 3h22L12 22"/></svg>
       </div>
     </button>`,
   );
